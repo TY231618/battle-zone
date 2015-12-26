a9e3fa21d9ff4e07f3f4c309a9091accc4a21c36
Tony Young <ty_training@hotmail.co.uk>
2015-12-26 19:18:23 +0000

    added a get play and renamed post play to enter_name

--- app.rb
+++ app.rb
@@ -8,9 +8,15 @@ class BattleZone < Sinatra::Base
     erb(:index)
   end
 
-  post '/play' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+  post '/enter_name' do
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
 
