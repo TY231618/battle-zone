a076d6617bee658c8834ad0f2db4b90fedc2ede2
Tony Young <ty_training@hotmail.co.uk>
2015-12-26 20:36:47 +0000

    User Story 3: attack player 2 and receive confirmation

---
+++ spec/attack_spec.rb
@@ -0,0 +1,14 @@
+require 'capybara'
+require_relative '../app.rb'
+
+
+  feature 'attack' do
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+
+    scenario 'allows a player to strike opponent' do
+      sign_up_and_play
+      click_button('Attack')
+      expect(page).to have_content('Tony attacked Robert')
+    end
+  end
