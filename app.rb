0bf183ddd9ce77928ad24a27772949a5ab762c90
Tony Young <ty_training@hotmail.co.uk>
2015-12-26 19:06:18 +0000

    removed sessions from post play and assigned params straight to instance variables

--- app.rb
+++ app.rb
@@ -9,10 +9,8 @@ class BattleZone < Sinatra::Base
   end
 
   post '/play' do
-    session1 = params[:player1]
-    session2 = params[:player2]
-    @player1 = session1
-    @player2 = session2
+    @player1 = params[:player1]
+    @player2 = params[:player2]
     erb(:play)
   end
 
