414aa02d61a31be7d06669f71fe95c2968c44119
Tony Young <ty_training@hotmail.co.uk>
2015-12-29 10:39:29 +0000

    Made game class smaller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,15 +1,8 @@
 class Game
 
   def initialize(player1, player2)
-    @players = [player1, player2]
-  end
-
-  def player1
-    @players.first
-  end
-
-  def player2
-    @players.last
+    @player1 = player1
+    @player2 = player2
   end
 
   def attack(player)
